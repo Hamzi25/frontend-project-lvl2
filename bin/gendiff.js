2ef01fbfd773d9c22fa956ca877d1534a626ce45
hamza torshkho <hamza.torshkho@gmail.com>
2022-07-16 06:22:14 +0300

    add format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,4 +7,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0');
 
+program
+  .option('-f, --format <type>', 'output format');
+
+program
+  .argument('<filepath1>')
+  .argument('<filepath2>');
+
 program.parse();
