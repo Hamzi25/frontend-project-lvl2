defca3a604bf58e5fc87ad1e6e2ef96fc34c6671
hamza torshkho <hamza.torshkho@gmail.com>
2022-09-27 09:35:42 +0300

    upadate

--- src/buildNewTree.js
+++ src/buildNewTree.js
@@ -6,39 +6,18 @@ const builder = (obj1, obj2) => {
   const arrayUniqKeys = _.sortBy(_.union(keys1, keys2));
   const diff = arrayUniqKeys.map((key) => {
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
-      return {
-        name: key,
-        value: builder(obj1[key], obj2[key]),
-        type: 'nested',
-      };
+      return { name: key, value: builder(obj1[key], obj2[key]), type: 'nested' };
     }
     if (_.isEqual(obj1[key], obj2[key])) {
-      return {
-        name: key,
-        value: obj1[key],
-        type: 'unchanged',
-      };
+      return { name: key, value: obj1[key], type: 'unchanged' };
     }
     if (!_.has(obj2, key)) {
-      return {
-        name: key,
-        value: obj1[key],
-        type: 'removed',
-      };
+      return { name: key, value: obj1[key], type: 'removed' };
     }
     if (!_.has(obj1, key)) {
-      return {
-        name: key,
-        value: obj2[key],
-        type: 'added',
-      };
+      return { name: key, value: obj2[key], type: 'added' };
     }
-    return {
-      name: key,
-      value1: obj1[key],
-      value2: obj2[key],
-      type: 'updated',
-    };
+    return { name: key, value1: obj1[key], value2: obj2[key], type: 'updated' };
   });
   return diff;
 };
