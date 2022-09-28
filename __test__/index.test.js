4e02291130dac607b74acd835f6257cbbcbf6b6e
hamza torshkho <hamza.torshkho@gmail.com>
2022-09-28 06:12:48 +0300

    Add txt results

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,16 +1,13 @@
 import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'node:path';
+import { dirname, resolve } from 'path';
 import { readFileSync } from 'node:fs';
 import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-console.log(__filename, '////');
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
-console.log(getFixturePath, '////');
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-console.log(readFile, '////');
 
 const formats = ['stylish', 'plain', 'json'];
 
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('0.8.0');
 
 program
   .option('-f, --format <type>', 'output format', 'stylish')
